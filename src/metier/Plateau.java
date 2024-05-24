bd718b4173084a38449fa76668d37153722381d4
betonfixe <clement.guelle@bbox.fr>
2024-05-24 17:05:30 +0200

    Création du plateau avec le positionnement de la pomme et du serpent

--- src/metier/Plateau.java
+++ src/metier/Plateau.java
@@ -1,2 +1,40 @@
 package src.metier;
 
+
+public class Plateau
+{
+
+    private char[][] plateau;
+    /**
+     *
+     * Constructeur de la classe Plateau
+     */
+    public Plateau()
+    {
+        this.plateau = initPlateau();
+    }
+
+
+    /**
+     * Création du plateau
+     * @return le plateau construit avec le serpent et un pomme
+     */
+    public char[][] initPlateau()
+    {
+        char[][] plateau = new char[32][32];
+
+        for( int lig = 0; lig < plateau.length; lig ++)
+            for( int col = 0; col < plateau[lig].length; col ++)
+                plateau[lig][col] = 'V';
+
+
+        plateau[16][10] = 'Q';
+        plateau[16][11] = 'C';
+        plateau[16][12] = 'T';
+
+        plateau[16][25] = 'P';
+
+
+        return plateau;
+    }
+}
